3f8c148ad4a6b92e2f148521eb6948468e67a739
ferozemohideen <fm87@duke.edu>
2018-07-27 22:38:39 -0500

    removed venv from other project

--- django-machine-learning/Scripts/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!C:\Users\Feroze\Google Drive\DUKE 2016-2020\AxoSim\django-machine-learning\Scripts\python.exe
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
